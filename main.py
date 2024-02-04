f485438e4042ba4f972f4861d8954ed7bdb0432f
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2024-02-04 15:52:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,24 +14,45 @@ bot = Client("santhu", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 def home():
     return render_template("index.html")
 
-@app.route('/get_channel_content', methods=['POST'])
-def get_channel_content():
-    channel_username = request.json.get('channelUsername')
+@app.route('/get_message_content', methods=['POST'])
+def get_message_content():
+    message_link = request.json.get('messageLink')
     try:
         with bot:
-            message = bot.get_chat_history(channel_username, limit=1)[0]
-            message_content = message.text if message.text else "No text content."
-            photos = [photo.file_id for photo in message.photo]
-            return jsonify({
-                'success': True,
-                'messageContent': message_content,
-                'photos': photos
-            })
+            message = get_message_details(message_link)
+            if message:
+                message_content = message.text if message.text else "No text content."
+                photos = [photo.file_id for photo in message.photo]
+                return jsonify({
+                    'success': True,
+                    'messageContent': message_content,
+                    'photos': photos
+                })
+            else:
+                return jsonify({
+                    'success': False,
+                    'errorMessage': 'Message not found.'
+                })
     except Exception as e:
         return jsonify({
             'success': False,
             'errorMessage': str(e)
         })
+        
+def get_message_details(message_link):
+    link_parts = message_link.split('/')
+    if len(link_parts) == 6 and link_parts[3] == 'c':
+        try:
+            chat_id = int(link_parts[4])
+            message_id = int(link_parts[5])
+            with bot:
+                chat = bot.get_chat(chat_id)
+                message = bot.get_messages(chat_id, message_id)       
+                return message
+        except (ValueError, IndexError):
+            return None
+    else:
+        return None
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
