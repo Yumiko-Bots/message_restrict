99241a3d4a19c82df9ccb41e49d643fe2bcfd948
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2024-02-04 13:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def get_channel_content():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
